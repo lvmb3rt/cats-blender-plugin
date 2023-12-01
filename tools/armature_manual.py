1834d3a49546ef23d9ed09bdea219d909ea19ba0
lvmb3rt <72808718+lvmb3rt@users.noreply.github.com>
2023-12-01 09:10:39 +0000

    Update armature_manual.py

--- tools/armature_manual.py
+++ tools/armature_manual.py
@@ -306,7 +306,7 @@ class PoseToRest(bpy.types.Operator):
                         scale_y_tmp *= parent.scale[1]
                         scale_z_tmp *= parent.scale[2]
 
-                        if armature.data.bones.get(parent.name).use_inherit_scale:
+                        if armature.data.bones.get(parent.name).inherit_scale:
                             scale_x_tmp, scale_y_tmp, scale_z_tmp = check_parent(parent, scale_x_tmp, scale_y_tmp,
                                                                                  scale_z_tmp)
 
